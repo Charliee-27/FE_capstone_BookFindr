44237bdf819075cd994dc6f054a494df525be937
mizingacharles0@gmail.com <mizingacharles0@gmail.com>
2025-01-11 15:15:52 +0200

    Serch Result Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import ContactUs from "./pages/ContactUs"
 import Home from "./pages/Home"
+import Search from "./pages/Search"
 
 
 export default function App() {
   return (
     < div className="">
-      <Home />
+      <Search />
       <ContactUs />
     </div>
   )
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -5,8 +5,8 @@ const ContactUs = () => {
     <main>
         <section className="bg-[url('public\images\About bg.png')] bg-cover bg-no-repeat bg-center px-10 h-50 " >
             <div className='content-center place-items-center py-20 container'>
-                <h1 className='h1 text-center font-poppins text-white '>About Us</h1> <br />
-                <p className='body-3 text-center font-poppins text-white '>We believe in making reading accessible to everyone. Our user-friendly platform helps you find and enjoy books that inspire you.</p>
+              <h1 className='h1 text-center font-poppins text-white '>About Us</h1> <br />
+              <p className='body-3 text-center font-poppins text-white '>We believe in making reading accessible to everyone. Our user-friendly platform helps you find and enjoy books that inspire you.</p>
             </div>
         </section>
     </main>
